30d10aa6fe83c4d88e12a3c32e9ddf7414dc90e1
後藤研一郎 <gotokenichiro@gotoukenichirounoMacBook-Pro.local>
2017-04-23 20:25:49 +0900

    correct ability.rb by deletion of else.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,6 @@ class Ability
       # user ||= User.new # guest user (not logged in)
       if user.admin?
         can :access, :rails_admin #grant access to rails_admin
-      else
         can :manage, :all         #allow superadmins to do anything
       end
     #
